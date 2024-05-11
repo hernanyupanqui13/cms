4ffcb8933ea0bb3caa28cb518aa13afdb4e076ee
hernanyupanqui13 <yup19001@byui.edu>
2024-05-10 22:26:31 -0500

    Adding navigation

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  @Output() selectedFeatureEvent: EventEmitter<string> = new EventEmitter<string>();
+
+  onSelected(selectedEvent: string) {
+    this.selectedFeatureEvent.emit(selectedEvent);
+  }
+
 
 }
