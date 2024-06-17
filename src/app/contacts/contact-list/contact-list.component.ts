a42cf06c8c4adc4ebcad94b5e2588a96237924e7
hernanyupanqui13 <yup19001@byui.edu>
2024-06-17 16:19:28 -0500

    Pipes working

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -6,24 +6,29 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  styleUrl: './contact-list.component.css',
 })
-export class ContactListComponent implements OnInit, OnDestroy{
-  public contacts :  Contact[] = []; 
-  private subscription : Subscription;
-  
+export class ContactListComponent implements OnInit, OnDestroy {
+  public contacts: Contact[] = [];
+  private subscription: Subscription;
+  term: string;
 
-  constructor(private contactsService: ContactsService) {
-  }
+  constructor(private contactsService: ContactsService) {}
 
-  ngOnInit() : void {
+  ngOnInit(): void {
     this.contacts = this.contactsService.getContacts();
-    this.subscription = this.contactsService.contactListChangedEvent.subscribe((contacts: Contact[]) => {
-      this.contacts = contacts;
-    });
+    this.subscription = this.contactsService.contactListChangedEvent.subscribe(
+      (contacts: Contact[]) => {
+        this.contacts = contacts;
+      }
+    );
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  search(value: string) {
+    this.term = value;
   }
 }
