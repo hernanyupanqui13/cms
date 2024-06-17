8e514896a75554aa7057e6dad49d8450005f2751
hernanyupanqui13 <yup19001@byui.edu>
2024-06-17 17:02:41 -0500

    Addign http features to contacts

--- src/app/contacts/contacts.service.ts
+++ src/app/contacts/contacts.service.ts
@@ -1,59 +1,70 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Contact } from './contact.model';
-import { MOCKCONTACTS } from './MOCKCONTACTS';
 import { Subject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactsService {
+  contacts: Array<Contact> = [];
+  contactSelectedEvent: EventEmitter<Contact> = new EventEmitter<Contact>();
+  contactListChangedEvent: Subject<Array<Contact>> = new Subject<
+    Array<Contact>
+  >();
+  private maxContactId: number;
+  private readonly BASE_URL =
+    'https://wdd430-cms-37ef7-default-rtdb.firebaseio.com/';
 
-  contacts : Array<Contact> = [];
-  contactSelectedEvent : EventEmitter<Contact> = new EventEmitter<Contact>();
-  contactListChangedEvent : Subject<Array<Contact>> = new Subject<Array<Contact>>();
-  private maxContactId : number;
-
-  constructor() { 
-    this.contacts = MOCKCONTACTS;
+  constructor(private readonly http: HttpClient) {
+    this.contacts = [];
     this.maxContactId = this.getMaxId();
   }
 
-
   public getContacts(): Contact[] {
+    this.http
+      .get<Contact[]>(`${this.BASE_URL}contacts.json`)
+      .subscribe((contacts: Contact[]) => {
+        this.contacts = contacts;
+        this.maxContactId = this.getMaxId();
+        this.contacts.sort((a, b) =>
+          a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+        );
+        this.contactListChangedEvent.next(this.contacts.slice());
+      });
     return this.contacts.slice();
   }
 
   public getContact(id: string): Contact {
-    return this.contacts.find(contact => contact.id === id)  || null;
-  } 
+    return this.contacts.find((contact) => contact.id === id) || null;
+  }
 
   private getMaxId(): number {
-    let maxId = 0
-    
+    let maxId = 0;
+
     for (let contact of this.contacts) {
-      let currentId = parseInt(contact.id)
+      let currentId = parseInt(contact.id);
       if (currentId > maxId) {
-        maxId = currentId
+        maxId = currentId;
       }
     }
 
-    return maxId
+    return maxId;
   }
 
-  public addContact(newContact: Contact) : void {
+  public addContact(newContact: Contact): void {
     if (!newContact) {
       return;
     }
 
     this.maxContactId++;
-    newContact.id = this.maxContactId + "";
+    newContact.id = this.maxContactId + '';
     this.contacts.push(newContact);
-    let contactListClone = this.contacts.slice();
-    this.contactListChangedEvent.next(contactListClone);
+    this.storeContacts();
   }
 
-  public updateContact(originalContact: Contact, newContact: Contact) : void {
-    if (!originalContact ||!newContact) {
+  public updateContact(originalContact: Contact, newContact: Contact): void {
+    if (!originalContact || !newContact) {
       return;
     }
 
@@ -64,12 +75,10 @@ export class ContactsService {
 
     newContact.id = originalContact.id;
     this.contacts[pos] = newContact;
-    let contactListClone = this.contacts.slice();
-    this.contactListChangedEvent.next(contactListClone);
+    this.storeContacts();
   }
 
-
-  public deleteContact(contact: Contact) : void {
+  public deleteContact(contact: Contact): void {
     if (!contact) {
       return;
     }
@@ -80,8 +89,17 @@ export class ContactsService {
     }
 
     this.contacts.splice(pos, 1);
-    let contactListClone = this.contacts.slice();
-    this.contactListChangedEvent.next(contactListClone);
+    this.storeContacts();
   }
-  
+
+  storeContacts(): void {
+    const contacts = JSON.stringify(this.contacts);
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    this.http
+      .put(`${this.BASE_URL}contacts.json`, contacts, { headers: headers })
+      .subscribe(() => {
+        this.contactListChangedEvent.next(this.contacts.slice());
+      });
+  }
 }
