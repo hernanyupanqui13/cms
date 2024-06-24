24e7dac0bc4d6fe8cc2811de8e0523c93f15411d
hernanyupanqui13 <yup19001@byui.edu>
2024-06-24 15:57:17 -0500

    Adding backend server

---
+++ server/routes/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'dist/cms/index.html'));
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CMS' })
+});
+
+module.exports = router;
